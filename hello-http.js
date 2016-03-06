25050fde5c8e1e85fbb0e7659a4a5ade66b66ff2
Burr Sutter <burrsutter@gmail.com>
2016-03-06 12:15:43 -0500

    Update hello-http.js
    Setup for Blue-Green demo

--- hello-http.js
+++ hello-http.js
@@ -1,6 +1,8 @@
 var http = require('http');
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello, Red Hat Developers World2! ' + Date.now() + "\n");
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<HTML><BODY BGCOLOR=BLUE>');
+    res.write('<FONT COLOR="WHITE">Hello, Red Hat Developers World! Host: ' + process.env.HOSTNAME  + '\n</FONT>');
+    res.end('</BODY></HTML>');
 }).listen(8000, '0.0.0.0');
 console.log('Server running at http://127.0.0.1:8000/');
